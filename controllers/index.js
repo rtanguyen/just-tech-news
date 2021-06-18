a505ff616864ffca7c636e57d7f8a157ffbec736
rtanguyen <rtanguyen@gmail.com>
2021-06-17 22:04:06 -0500

    add post

--- controllers/index.js
+++ controllers/index.js
@@ -4,9 +4,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 //if we make a request to any endpoint that doesn't exist, we'll receive a 404 error
 router.use((req, res) => {
